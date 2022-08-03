65712790566df86c70d44a01b5e328e83709a0b0
Nicholas Ladjevich <zantladjevich@gmail.com>
2022-08-02 21:42:09 -0400

    Added US travel path. Added else statement in case SE or US is'nt selected. Added a confirmation at the end of each travel path.

--- day-trip.py
+++ day-trip.py
@@ -2,12 +2,12 @@ import random
 
 places_to_vist_US = ["New York City", "Seattle", "Chicago", "Miami"]
 places_to_vist_SE_Asia = ["Saigon", "Singapore", "Bali", "Hanoi"]
-things_to_do_US = ["City tour", "Try out expensive resturuants", "Go to the casino", "Go to a concert"]
-things_to_do_SE_Asia = ["Going on a boat tour", "Going hiking", "doing a Street food tour", "Visiting a religious temple"]
-transportation_US = ["Car", "Train", "Plane"]
-transportation_SE_Asia = ["Boat", "Plane"]
-places_to_stay_US = ["AirBnB", "Hotel", "Motel"]
-places_to_stay_SE_Asia = ["Hotel", "Hostel", "AirBnB"]
+things_to_do_US = ["doing a City tour", "trying out expensive resturuants", "going to the casino", "going to a concert"]
+things_to_do_SE_Asia = ["going on a boat tour", "going hiking", "doing a Street food tour", "visiting a religious temple"]
+transportation_US = ["car", "train", "plane"]
+transportation_SE_Asia = ["boat", "plane"]
+places_to_stay_US = ["airBnB", "hotel", "motel"]
+places_to_stay_SE_Asia = ["hotel", "hostel", "airBnB"]
 main_loop = True
 
 def random_list_selector(list):
@@ -124,6 +124,60 @@ while main_loop == True:
         elif confirmation == "Yes":
             print("Ok! Enjoy your trip.")
             main_loop == False
+    elif country_select == "US":
+        US_visit = True
+        while US_visit == True:
+            US_visit_random = random_list_selector(places_to_vist_US)
+            US_visit_user_select = input(f"Ok! Would you like to visit {US_visit_random}? Yes or No. ")
+            if US_visit_user_select == "Yes":
+                print(f"You will be visiting {US_visit_random}")
+                US_visit = False
+            elif US_visit_user_select == "No":
+                continue
+            else:
+                print("Please enter Yes or No.")
+        US_activity = True
+        while US_activity == True:
+            US_activity_random = random_list_selector(things_to_do_US)
+            US_activity_user_select = input(f"Ok! Does {US_activity_random} sound good for your main activity? Yes or No. ")
+            if US_activity_user_select == "Yes":
+                print(f"You will be {US_activity_random}.")
+                US_activity = False
+            elif US_activity_user_select == "No":
+                continue
+            else:
+                print("Please enter Yes or No.")
+        US_transportation = True
+        while US_transportation == True:
+            US_transportation_random = random_list_selector(transportation_US)
+            US_transportation_user_select = input(f"Ok! Would you like to travel by {US_transportation_random}? Yes or No. ")
+            if US_transportation_user_select == "Yes":
+                print(f"You will be travelling by a {US_transportation_random}.")
+                US_transportation = False
+            elif US_transportation_user_select == "No":
+                continue
+            else:
+                print("Please enter Yes or No.")
+        US_accomodation = True
+        while US_accomodation == True:
+            US_accomodation_random = random_list_selector(places_to_stay_US)
+            US_accomodation_user_select = input(f"Ok! Would you like to stay in a {US_accomodation_random}? Yes or No. ")
+            if US_accomodation_user_select == "Yes":
+                print(f"You will be staying in a {US_accomodation_random}.")
+                US_accomodation = False
+            elif US_accomodation_user_select == "No":
+                continue
+            else:
+                print("Please enter Yes or No.")
+        confirmation = input(f"Ok! You will travel to {US_visit_random}. You will be travelling by {US_transportation_random}. You will be staying in a {US_accomodation_random}. And you will be {US_activity_random} for your main activity. Does this sound good? Yes or No. ")
+        if confirmation == "No":
+            print("Ok! Lets try again.")
+            continue
+        elif confirmation == "Yes":
+            print("Ok! Enjoy your trip.")
+            main_loop == False
+    else:
+        print("Please select SE Asia or US.")
 
 
     
